b6ada7288a62064ac4981798aedb9d4b199b8859
Aaron <Bowles@Aarons-MacBook-Pro.local>
2019-11-07 12:51:57 -0500

    created battles urls

--- wars/urls.py
+++ wars/urls.py
@@ -8,6 +8,12 @@ urlpatterns = [
     path('wars/new', views.war_create, name='war_create'),
     path('wars/<int:pk>/edit', views.war_edit, name='war_edit'),
     path('wars/<int:pk>/delete', views.war_delete, name='war_delete'),
+
+    path('battles', views.battle_list, name='battle_list')battlepath('battles/<int:pk>', views.war_detail, name='battle_detail'),
+    path('battles/new', views.war_create, name='battle_create'),
+    path('battles/<int:pk>/edit', views.war_edit, name='battle_edit'),
+    path('battles/<int:pk>/delete', views.war_delete, name='battle_delete'),
+
 ]
 
 # urlpatterns = [
