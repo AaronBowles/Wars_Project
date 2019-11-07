89dd27ae183bd4caa90f8edf6ea80cc906f89e76
Aaron <Bowles@Aarons-MacBook-Pro.local>
2019-11-07 16:07:36 -0500

    made model migrations to create array field for key technologies

--- wars/models.py
+++ wars/models.py
@@ -1,14 +1,16 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 
 class War(models.Model):
     name = models.CharField(max_length=200)
     major_players = models.CharField(max_length=1000)
-    key_technologies = models.CharField(max_length=500)
+    key_technologies = ArrayField(models.CharField(max_length=500))
     winner = models.CharField(max_length=500)
     start_date = models.CharField(max_length=50)
     end_date = models.CharField(max_length=50)
+
 
     def __str__(self):
         return self.name
@@ -18,7 +20,7 @@ class Battle(models.Model):
     name = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
     major_players = models.CharField(max_length=1000)
-    key_technologies = models.CharField(max_length=500)
+    key_technologies = ArrayField(models.CharField(max_length=500))
     winner = models.CharField(max_length=500)
 
     def __str__(self):
