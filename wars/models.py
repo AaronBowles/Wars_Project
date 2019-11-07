5be91ace6399bda8e64c43fecef64321f6572972
Aaron <Bowles@Aarons-MacBook-Pro.local>
2019-11-06 21:15:45 -0500

    complete setup

---
+++ wars/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Wars(models.Model):
+    name = models.CharField(max_length=200)
+    major_players = models.CharField(max_length=100)
+    key_technologies = models.CharField(max_length=500)
+    winner = models.CharField(max_length=500)
+    start_date = models.CharField(max_length=50)
+    end_date = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
