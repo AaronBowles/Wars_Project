57c0bacda1f52bef153ff801284af46e337946c7
Aaron <Bowles@Aarons-MacBook-Pro.local>
2019-11-06 22:27:27 -0500

    editted models

--- wars/models.py
+++ wars/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Wars(models.Model):
+class War(models.Model):
     name = models.CharField(max_length=200)
     major_players = models.CharField(max_length=100)
     key_technologies = models.CharField(max_length=500)
