05f7958d3088e60a62522164e8dcd0d065768e8e
Aaron <Bowles@Aarons-MacBook-Pro.local>
2019-11-07 12:39:58 -0500

    completed migration for War model logic adjustment and created Battle Model

--- wars/models.py
+++ wars/models.py
@@ -4,11 +4,22 @@ from django.db import models
 
 class War(models.Model):
     name = models.CharField(max_length=200)
-    major_players = models.CharField(max_length=100)
+    major_players = models.CharField(max_length=1000)
     key_technologies = models.CharField(max_length=500)
     winner = models.CharField(max_length=500)
     start_date = models.CharField(max_length=50)
     end_date = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Battle(models.Model):
+    war = models.ForeignKey(War, on_delete=models.CASCADE, related_name='battles')
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    major_players = models.CharField(max_length=1000)
+    key_technologies = models.CharField(max_length=500)
+    winner = models.CharField(max_length=500)
 
     def __str__(self):
         return self.name
