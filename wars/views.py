6dd6eb17b47a9a48a8c3daeacdc8c6c8286c273b
Aaron <Bowles@Aarons-MacBook-Pro.local>
2019-11-07 11:37:28 -0500

    created CRUD views for War model

--- wars/views.py
+++ wars/views.py
@@ -10,23 +10,34 @@ def war_list(request):
     return render(request, 'wars/war_list.html', {'wars': wars})
 
 def war_detail(request, pk):
-    war = War.objects.get(id=pk)
+    war = War.objects.get(id = pk)
     return render(request, 'wars/war_detail.html', {'war': war})
 
 def war_create(request):
     if request.method = 'POST':
         form = WarForm(request.POST)
         if form.is_valid():
-            artist = form.save()
-            return redirect('war_detail', pk=war.pk)
+            war = form.save()
+            return redirect('war_detail', pk = war.pk)
     else:
         from = WarForm()
-    return render(request, 'war/war_form.html', {'form': form})
+    return render(request, 'wars/war_form.html', {'form': form})
 
 
+def war_edit(request, pk):
+    war = War.objects.get(pk = pk)
+    if request.method = 'POST':
+        form = WarForm(request.POST, instance = war)
+        if form.is_valid():
+            war = form.save()
+            return redirect('war_detail', pk = war.pk)
+    else:
+        form = WarForm(instance = war)
+    return render(request, 'wars/war_form.html', {'form': form})
 
-
-
+def war_delete(request, pk):
+    War.objects.get(id=pk).delete()
+    return redirect('war_list')
 
 # class WarList(generics.ListCreateAPIView):
 #     queryset = War.objects.all()
