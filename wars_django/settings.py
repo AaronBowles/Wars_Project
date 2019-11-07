a461aefdac8caa317ef2a40194684a5a7feaa964
Aaron <Bowles@Aarons-MacBook-Pro.local>
2019-11-07 00:14:45 -0500

    adjusted logic

--- wars_django/settings.py
+++ wars_django/settings.py
@@ -28,9 +28,9 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 REST_FRAMEWORK = {
-        'DEFAULT_PERMISSION_CLASSES': [
-            'rest_framework.permissions.DjangoModelPersmissionsOrAnonReadOnly'
-        ]
+       'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
 }
 # Application definition
 
